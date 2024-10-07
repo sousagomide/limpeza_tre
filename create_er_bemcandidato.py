d8a0709cef3e9f0499de2a284e50771917dd63cf
smp-ifgoiano <smp.ifgoiano@gmail.com>
2024-10-07 11:31:48 -0300

    update

--- create_er_bemcandidato.py
+++ create_er_bemcandidato.py
@@ -16,6 +16,10 @@ for coluna in df.columns:
         df[coluna] = df[coluna].apply(lambda x: str(x).replace("'", "") if pd.notnull(x) else x)
 print("Acentos removidos com sucesso de todas as colunas do DataFrame.")
 
+# Converte VR_BEM_CANDIDATO para valor double
+df['VR_BEM_CANDIDATO'] = df['VR_BEM_CANDIDATO'].str.replace(',', '.').astype(float)
+print("Coluna VR_BEM_CANDIDATO convertida para valor double com sucesso.")
+
 columns = ['SQ_CANDIDATO', 'DS_BEM_CANDIDATO', 'VR_BEM_CANDIDATO']
 df_bem_candidato = df[columns]
 
@@ -23,8 +27,9 @@ df_bem_candidato = df[columns]
 # Gera o arquivo SQL
 with open('insert_bem_candidato.sql', 'w', encoding='utf-8') as f:
     for _, row in df_bem_candidato.iterrows():
-        valores = ', '.join([f"'{str(row[col]).replace("'", "''")}'" for col in columns])
-        f.write(f"INSERT INTO bem_candidato (SQ_CANDIDATO, DS_BEM_CANDIDATO, VR_BEM_CANDIDATO) VALUES ({valores});\n")
+        valores = [f"'{str(row[col]).replace("'", "''")}'" if col != 'VR_BEM_CANDIDATO' else str(row[col]) for col in columns]
+        valores_str = ', '.join(valores)
+        f.write(f"INSERT INTO bem_candidato (SQ_CANDIDATO, DS_BEM_CANDIDATO, VR_BEM_CANDIDATO) VALUES ({valores_str});\n")
 print("Arquivo SQL 'insert_bem_candidato.sql' gerado com sucesso.")
 
 
